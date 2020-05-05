8e00deaf60e3f01f820da9c3a372b476133b2487
freemanzx <freemanzx@Beast.localdomain>
2020-05-05 19:09:11 +0100

    Improved list-selectors

--- commands/list-selectors.js
+++ commands/list-selectors.js
@@ -3,27 +3,42 @@ var colors = require('colors'),
   fs = require('fs')
 
 module.exports = function (program) {
+
+  function printExchangeDetails(exchange, asset) {
+    var allPproducts = require(`../extensions/exchanges/${exchange}/products.json`)
+    let products = asset !== undefined ? allPproducts.filter((pair) => { return pair.asset === asset; }) : allPproducts;
+    if (products.length) {
+      products.sort(function (a, b) {
+        if (a.asset < b.asset) return -1
+        if (a.asset > b.asset) return 1
+        if (a.currency < b.currency) return -1
+        if (a.currency > b.currency) return 1
+        return 0
+      })
+  
+      console.log(`${exchange}:`)
+      products.forEach(function (p) {
+        console.log('  ' + exchange.cyan + '.'.grey + p.asset.green + '-'.grey + p.currency.cyan + (p.label ? ('   (' + p.label + ')').grey : ''))
+      })
+    }
+  }
+  
   program
     .command('list-selectors')
+    .option('-e, --exchange <exchange>', 'Select single exchange')
+    .option('-a, --asset <asset>', 'Select single asset')
     .description('list available selectors')
-    .action(function (/*cmd*/) {
-      var exchanges = fs.readdirSync('./extensions/exchanges')
-      exchanges.forEach(function(exchange){
-        if (exchange === 'sim' || exchange === '_stub') return
-
-        console.log(`${exchange}:`)
-        var products = require(`../extensions/exchanges/${exchange}/products.json`)
-        products.sort(function (a, b) {
-          if (a.asset < b.asset) return -1
-          if (a.asset > b.asset) return 1
-          if (a.currency < b.currency) return -1
-          if (a.currency > b.currency) return 1
-          return 0
+    .action((cmdObj) => {
+      if (cmdObj.exchange !== undefined) {
+        printExchangeDetails(cmdObj.exchange, cmdObj.asset)
+      } else {
+        var exchanges = fs.readdirSync('./extensions/exchanges')
+        exchanges.forEach(function(exchange){
+          if (exchange === 'sim' || exchange === '_stub') return
+          printExchangeDetails(exchange, cmdObj.asset)
         })
-        products.forEach(function (p) {
-          console.log('  ' + exchange.cyan + '.'.grey + p.asset.green + '-'.grey + p.currency.cyan + (p.label ? ('   (' + p.label + ')').grey : ''))
-        })
-      })
+      }
       process.exit()
     })
 }
+
